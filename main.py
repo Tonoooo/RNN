d3a60e232de8c47742131a48c77ebe1c0c186bd3
Tonoooo <92434748+Tonoooo@users.noreply.github.com>
2021-12-28 18:08:20 +0700

    mengahapus CuDNNLSTM

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras.layers import LSTM # jika runtime biasa
-# from tensorflow.compat.v1.keras.layers import CuDNNLSTM # jika runtime GPU
+from tensorflow.keras.layers import LSTM
 
 
 mnist = tf.keras.datasets.mnist  # mnist adalah kumpulan data 28x28 gambar angka tulisan tangan dan labelnya
@@ -24,16 +23,6 @@ model.add(Dense(32, activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(10, activation='softmax'))
 
-# JIKA Anda menjalankan dengan GPU, cobalah jenis lapisan CuDNNLSTM sebagai gantinya (jangan lewati activation, tanh diperlukan) dan ini akan jauh lebih cepat dan lebih efisien
-"""
-model.add(CuDNNLSTM(128, input_shape=(x_train.shape[1:]), return_sequences=True))
-model.add(Dropout(0.2))
-model.add(CuDNNLSTM(128))
-model.add(Dropout(0.1))
-model.add(Dense(32, activation='relu')) # boleh dicoba activation='tanh'
-model.add(Dropout(0.2))
-model.add(Dense(10, activation='softmax'))
-"""
 
 opt = tf.keras.optimizers.Adam(lr=0.001, decay=1e-6)
 
